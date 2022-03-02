a5e8fe24055368768d724fd6cec7c7a64a79d6ff
Luigi <lordwells79@hotmail.it>
2022-03-02 16:36:54 +0100

    bootstrap e fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-ums';
+  faPencilAlt = faPencilAlt;
 }
